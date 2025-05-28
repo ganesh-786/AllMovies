5b36dfd37d98141fb64e800e337f0ab12239de67
Ganesh Chaudhary <139463065+ganesh-786@users.noreply.github.com>
2025-05-28 11:00:55 +0545

    update vite.config
    this is update in the world

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // or vue(), svelte(), etc.
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // your plugins
+  build: {
+    outDir: 'build' // <--- Add or change this line
+  }
 })
